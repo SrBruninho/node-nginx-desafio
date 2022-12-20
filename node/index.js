f4470c9c219244f71cff210bbda09624924b2581
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-12-20 12:27:08 -0300

    Apresentação da lista dos nomes inseridos

--- node/index.js
+++ node/index.js
@@ -11,28 +11,27 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection( config );
 
-const { uniqueNamesGenerator, adjectives, colors, names } = require('unique-names-generator');
-
-const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, names] });
-
+const { uniqueNamesGenerator, names, colors } = require('unique-names-generator');
 
 var peopleList = [];
-const insert_sql = "INSERT INTO people(name) values('"+randomName+"')";
-
-
-connection.connect(function(err) {
-    if (err) throw err;
-    connection.query("SELECT * FROM people", function (err, result, fields) {
-      if (err) throw err;
-      result.forEach(function(entry) {
-        peopleList.push(entry.name);
-    });
-    });
-  });
-  connection.query(insert_sql);
 
 app.get( '/',(req,res) =>{
-   res.send("<html><body><h1>Full Cycle Rocks!</h1></body>"+peopleList+"</html>");
+    const randomName = uniqueNamesGenerator({ dictionaries: [names, colors] });
+    const insert_sql = "INSERT INTO people(name) values('"+randomName+"')";
+    
+    connection.query(insert_sql);
+
+    connection.query("SELECT name FROM people", function (err, result, fields) {
+       peopleList = result;        
+    });  
+
+    res.write("<h1>Full Cycle Rocks!</h1>");
+
+    peopleList.forEach(p => {
+            res.write("<li>"+p.name+"</li>");
+        });
+
+    res.end();
 });
 
 app.listen(port, ()=>{
